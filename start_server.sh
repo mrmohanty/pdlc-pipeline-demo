684fb4d9d2e07d3d8fb1de2ab4c33b994e1f1897
mrmohanty <37418356+mrmohanty@users.noreply.github.com>
2025-07-14 17:25:41 +0530

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 echo "Starting the Python web server..."
-cd /home/ec2-user/app
+
+cd /home/ec2-user/app || exit 1
+
+# Stop any previously running app
+pkill -f app.py || true
+
+# Start app in background
 nohup python3 app.py > output.log 2>&1 &
